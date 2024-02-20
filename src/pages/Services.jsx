abaf28e28677d9aa2c477c651b63585867a406d1
Pngtaq <raisonsalvador0@gmail.com>
2024-02-20 10:32:46 +0800

    header and services

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,7 +1,38 @@
 function Services() {
   return (
-    <div>
-      <h1>Services</h1>
+    <div className="flex flex-wrap gap-5 justify-center">
+      <div className="space-y-6 border-8 border-orange-400 px-6 py-2">
+        <div className="flex justify-center  ">
+          <img
+            className="rounded-full w-40 h-40 border-4 border-dashed border-orange-300"
+            src="https://i.pinimg.com/236x/2a/7d/4c/2a7d4c4bc1381a476b8b8a85885ac392.jpg"
+            alt="person-1"
+          />
+        </div>
+        <div>
+          <h3>Name: Erl Salvador</h3>
+          <p>Information about the person</p>
+          <p>Information about the person</p>
+          <p>Information about the person</p>
+        </div>
+      </div>
+      <div className="space-y-6 border-8 border-orange-400 px-6 py-2">
+        <div className="flex justify-center  ">
+          <img
+            className="rounded-full w-40 h-40 border-4 border-dashed border-orange-300"
+            src="https://i.pinimg.com/236x/2a/7d/4c/2a7d4c4bc1381a476b8b8a85885ac392.jpg"
+            alt="person-1"
+          />
+        </div>
+        <div>
+          <h3>Name: Erl Salvador</h3>
+          <p>Information about the person</p>
+          <p>Information about the person</p>
+          <p>Information about the person</p>
+        </div>
+      </div>
     </div>
   );
 }
+
+export default Services;
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -2,7 +2,7 @@ import { NavLink } from "react-router-dom";
 
 function Header() {
   return (
-    <header className="flex items-center justify-around border-b-2 py-3 bg-orange-400 font-semibold uppercase text-md">
+    <header className="flex items-center justify-around border-b-2 py-5 bg-orange-400 font-semibold uppercase text-xl">
       <NavLink to="home">Company name</NavLink>
 
       <NavLink to="login">Login</NavLink>
